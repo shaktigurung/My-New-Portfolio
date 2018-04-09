ef3de6b6e1b8e4581d061eaefe3d671ad2eb3c2f
shakti gurung <mr.shakti.gurung@gmail.com>
2018-04-09 13:21:12 +1000

    Skill bar with icon

--- js/script.js
+++ js/script.js
@@ -27,3 +27,16 @@ $(function () {
     });
 
 });
+
+$(document).ready(function(){
+        		$('.html').animate({width:'90%'},2000);
+        		$('.css').animate({width:'80%'},2000);
+        		$('.js').animate({width:'70%'},2000);
+        		$('.php').animate({width:'75%'},2000);
+        		$('.jquery').animate({width:'70%'},2000);
+                $('.bootstrap').animate({width:'85%'},2000);
+                $('.drupal').animate({width:'80%'},2000);
+                $('.wordpress').animate({width:'75%'},2000);
+                $('.git').animate({width:'80%'},2000);
+                $('.github').animate({width:'80%'},2000);
+        	});
